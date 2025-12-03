0f6307a91a219dca71f3393d559919240a10c886
amrit <amrit.aryal@yarsa.tech>
2025-12-03 15:37:57 +0545

    not dj

--- templates.py
+++ templates.py
@@ -29,7 +29,7 @@ def generate_html_page(current_track):
     <!DOCTYPE html>
     <html>
     <head>
-        <title>Yarsa MP3 Streamer DJ</title>
+        <title>Yarsa MP3 Streamer</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <script src="https://cdn.tailwindcss.com"></script>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
